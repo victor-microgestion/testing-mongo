c2dce56122757a25abe8b2af5f69c0e4960b5940
victor-microgestion <victor_villafane@microgestion.com>
2025-03-26 18:47:33 -0300

    Nuevos metodos GET, POST, PUT, DELETE

--- src/main/java/com/testing/rest/ControllerApi.java
+++ src/main/java/com/testing/rest/ControllerApi.java
@@ -1,10 +1,16 @@
 package com.testing.rest;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
+
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -24,6 +30,48 @@ public class ControllerApi {
     public Response getMock() {
         List<Users> users = usersRepository.findAll().toList();
         return Response.ok(users).build();
+    }
+
+    //id 
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUserById(@PathParam("id") String id) {
+        Optional<Users> user = usersRepository.findById(new ObjectId(id));
+        if (user != null) {
+            return Response.ok(user).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Usuario no encontrado con ID: " + id)
+                    .build();
+        }
+    }
+
+    // POST: Crear un nuevo usuario
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createUser(Users user) {
+        usersRepository.save(user);
+        return Response.status(Response.Status.CREATED)
+                .entity("Usuario creado: " + user.getEmail())
+                .build();
+    }
+
+    // DELETE: Eliminar un usuario por correo electrónico
+    @DELETE
+    @Path("/{email}")
+    @RolesAllowed({ "admin" })
+    public Response deleteUserByEmail(@PathParam("email") String email) {
+        Users user = usersRepository.findByEmail(email);
+        if (user != null) {
+            usersRepository.delete(user);
+            return Response.ok("Usuario eliminado: " + email).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Usuario no encontrado con email: " + email)
+                    .build();
+        }
     }
 
     @PUT
